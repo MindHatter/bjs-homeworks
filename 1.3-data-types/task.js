cff9bb6087c3bdd21e974e2df414e9149593f79d
Aleksandr Menshikov <menshikov.abz@gmail.com>
2020-10-27 22:02:05 +0300

    tasks 1.3 is solved

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,26 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent)) {
+        return `Параметр 'Процентная ставка' содержит неправильное значение ${percent}.`;
+    }
+    else if (isNaN(contribution)) {
+        return `Параметр 'Первоначальный взнос' содержит неправильное значение ${contribution}.`;
+    }
+    else if (isNaN(amount)) {
+        return `Параметр 'Сумма кредита' содержит неправильное значение ${amount}.`;
+    }
+
+    let S = amount - contribution;
+    let P = percent / 100 / 12;
+    let n = (date.getFullYear() - new Date().getFullYear()) * 12;
+    let monthlyPayment = S * (P + P / ((Math.pow((1 + P), n)) - 1 ));
+    let totalAmount = monthlyPayment * n;
+    console.log(monthlyPayment);
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = `Привет, мир! Меня зовут ${(name === undefined || name === null || name === '') ? 'Аноним' : name}`;
+    return greeting;
 }
